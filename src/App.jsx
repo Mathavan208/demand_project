ad2245c28bce8d35867f5c0d6978d6643e2a0653
Mathavan208 <210701154@rajalakshmi.edu.in>
2025-08-11 06:47:50 +0530

    mmm

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import RazorpayCheckout from './RazorpayCheckout'
+function App() {
+ 
+
+  return (
+    <>
+      <RazorpayCheckout />
+    </>
+  )
+}
+
+export default App
